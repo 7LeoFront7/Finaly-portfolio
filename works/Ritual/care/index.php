23f7cdf8283da1fb91163850db6410bcbb16c4cf
Максим <98467035+7LeoFront7@users.noreply.github.com>
2022-06-09 00:17:01 +0300

    Add files via upload

---
+++ works/Ritual/care/index.php
@@ -0,0 +1,55 @@
+<?include $_SERVER['DOCUMENT_ROOT'].'/header.php'?>
+
+<div class="container">
+
+    <section class="service__callback p30-0">
+        <h2>качественный уход и обслуживание захоронений</h2>
+        <a href="#feedback" data-fancybox >заказать консультацию</a>
+    </section>
+
+    <section class="catalog p30-0">
+        <h2>уход и обслуживание</h2>
+        <div class="row catalog-items">
+            <div class="catalog-item col-12 col-sm-6 col-md-4">
+                <div class="images-area"></div>
+                <a href="" class="link-area"></a>
+            </div>
+            <div class="catalog-item col-12 col-sm-6 col-md-4">
+                <div class="images-area"></div>
+                <a href="" class="link-area"></a>
+            </div>
+            <div class="catalog-item col-12 col-sm-6 col-md-4">
+                <div class="images-area"></div>
+                <a href="" class="link-area"></a>
+            </div>
+            <div class="catalog-item col-12 col-sm-6 col-md-4">
+                <div class="images-area"></div>
+                <a href="" class="link-area"></a>
+            </div>
+            <div class="catalog-item col-12 col-sm-6 col-md-4">
+                <div class="images-area"></div>
+                <a href="" class="link-area"></a>
+            </div>
+            <div class="catalog-item col-12 col-sm-6 col-md-4">
+                <div class="images-area"></div>
+                <a href="" class="link-area"></a>
+            </div>
+            <div class="catalog-item col-12 col-sm-6 col-md-4">
+                <div class="images-area"></div>
+                <a href="" class="link-area"></a>
+            </div>
+            <div class="catalog-item col-12 col-sm-6 col-md-4">
+                <div class="images-area"></div>
+                <a href="" class="link-area"></a>
+            </div>
+            <div class="catalog-item col-12 col-sm-6 col-md-4">
+                <div class="images-area"></div>
+                <a href="" class="link-area"></a>
+            </div>
+        </div>
+        <div class="catalog-arrows"></div>
+    </section>
+
+</div>
+
+<?include $_SERVER['DOCUMENT_ROOT'].'/footer.php'?>
