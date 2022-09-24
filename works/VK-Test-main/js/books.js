c11ea58cadd4638585e22bc3106a7f2991b2563f
Максим <98467035+7LeoFront7@users.noreply.github.com>
2022-09-24 17:13:24 +0300

    Add files via upload

---
+++ works/VK-Test-main/js/books.js
@@ -0,0 +1,657 @@
+let arrBoksRaz = [
+    
+    {"name":"Чистый код: создание, анализ и рефакторинг",
+    "authorName":"Роберт Мартин &#169;",
+    "price":700,
+    "coverUrl":"http://45.8.249.57/public/6189288048.webp",
+    "categoryId":3,
+    "idBook": 5},
+    
+ 
+    
+    {"name":"Идеальный программист",
+    "authorName":"Роберт Мартин &#169;",
+    "price":1020,"coverUrl":"http://45.8.249.57/public/6264960718.webp",
+    "categoryId":3,
+    "idBook": 7},
+   
+    
+  
+    
+    {"name":"Совершенный код",
+    "authorName":"Стив Макконнелл &#169;","price":1787,
+    "coverUrl":"http://45.8.249.57/public/6190014646.webp",
+    "categoryId":3,
+    "idBook": 11},
+    
+   
+    
+    
+    {"name":"Код: тайный язык информатики",
+    "authorName":"Чарльз Петцольд &#169;",
+    "price":2479,"coverUrl":"http://45.8.249.57/public/6292446592.webp",
+    "categoryId":3,
+    "idBook": 15},
+    
+    {"name":"Программист-прагматик","authorName":"Дэвид Томас, Эндрю Хант &#169;",
+    "price":3495,
+    "coverUrl":"http://45.8.249.57/public/6016016523.webp",
+    "categoryId":3,
+    "idBook": 16},
+    
+    {"name":"Шаблоны корпоративных приложений","authorName":"Мартин Фаулер &#169;",
+    "price":4295,
+    "coverUrl":"http://45.8.249.57/public/6225046249.webp",
+    "categoryId":3,
+    "idBook": 17},
+    
+    {"name":"Рефакторинг. Улучшение проекта, существующего кода",
+    "authorName":"Мартин Фаулер &#169;",
+    "price":7125,
+    "coverUrl":"http://45.8.249.57/public/6114690274.webp",
+    "categoryId":3,
+    "idBook": 18},
+    
+    {"name":"Алгоритмы. Построение и анализ",
+    "authorName":"Томас Кормен, Чарльз Лейзерсон, Рональд Ривест, Клиффорд Штайн &#169;",
+    "price":16875,"coverUrl":"http://45.8.249.57/public/6120969471.webp",
+    "categoryId":3,
+    "idBook": 19}]
+
+let arrBoksUpr = [
+
+    
+    {"name":"Управление проектами от А до Я",
+    "authorName":"Ричард Ньютон &#169;",
+    "price":675,"coverUrl":"http://45.8.249.57/public/6083509554.webp",
+    "categoryId":2,
+    "idBook": 4},
+    
+    
+    
+    {"name":"Бизнес с нуля",
+    "authorName":"Эрик Рис &#169;",
+    "price":920,
+    "coverUrl":"http://45.8.249.57/public/6304182856.webp",
+    "categoryId":2,
+    "idBook": 6},
+    
+    
+    
+   
+    
+    {"name":"Как пасти котов",
+    "authorName":"Дж.Ханк Рейнвотер &#169;",
+    "price":1207,
+    "coverUrl":"http://45.8.249.57/public/6277282274.webp",
+    "categoryId":2,
+    "idBook": 9},
+    
+    {"name":"Пять пороков команды",
+    "authorName":"Патрик Ленсиони &#169;",
+    "price":1649,"coverUrl":"http://45.8.249.57/public/6206226336.webp",
+    "categoryId":2,
+    "idBook": 10},
+    
+    
+    
+    {"name":"Scrum. Революционный метод управления проектами",
+    "authorName":"Джефф Сазерленд &#169;",
+    "price":1937,
+    "coverUrl":"http://45.8.249.57/public/6280422562.webp",
+    "categoryId":2,
+    "idBook": 12},
+    
+    
+    
+    {"name":"Deadline. Роман об управлении проектами",
+    "authorName":"Том Демарко &#169;",
+    "price":2448,
+    "coverUrl":"http://45.8.249.57/public/6277899275.webp",
+    "categoryId":2,
+    "idBook": 14},
+    
+    
+   
+]
+
+let arrBoksDiz = [{
+    "name":"Отзывчивый веб-дизайн",
+    "authorName":"Итан Маркотт &#169;",
+    "price":349,
+    "coverUrl":"http://45.8.249.57/public/6012485021.webp",
+    "categoryId":1,
+    "idBook": 1},
+    
+    {"name":"Эмоциональный веб-дизайн",
+    "authorName":"Аарон Уолтер &#169;",
+    "price":349,
+    "coverUrl":"http://45.8.249.57/public/6012485032.webp",
+    "categoryId":1,
+    "idBook": 2},
+    
+    {"name":"Дизайн — это работа",
+    "authorName":"Майк Монтейро &#169;",
+    "price":349,
+    "coverUrl":"http://45.8.249.57/public/6013099362.webp",
+    "categoryId":1,
+    "idBook": 3},
+    
+    
+ 
+    
+
+    
+    {"name":"Не заставляйте меня думать",
+    "authorName":"Стив Круг &#169;",
+    "price":1169,
+    "coverUrl":"http://45.8.249.57/public/6053518393.webp",
+    "categoryId":1,
+    "idBook": 8},
+  
+    
+ 
+    
+    {"name":"Модульные системы в графическом дизайне",
+    "authorName":"Йозеф Мюллер-Брокманн &#169;",
+    "price":2048,
+    "coverUrl":"http://45.8.249.57/public/6252844739.webp",
+    "categoryId":1,
+    "idBook": 13},
+    
+ 
+    
+    
+ 
+]
+
+let arrBoks = [{
+"name":"Отзывчивый веб-дизайн",
+"authorName":"Итан Маркотт &#169;",
+"price":349,
+"coverUrl":"http://45.8.249.57/public/6012485021.webp",
+"categoryId":1,
+"idBook": 1},
+
+{"name":"Эмоциональный веб-дизайн",
+"authorName":"Аарон Уолтер &#169;",
+"price":349,
+"coverUrl":"http://45.8.249.57/public/6012485032.webp",
+"categoryId":1,
+"idBook": 2},
+
+{"name":"Дизайн — это работа",
+"authorName":"Майк Монтейро &#169;",
+"price":349,
+"coverUrl":"http://45.8.249.57/public/6013099362.webp",
+"categoryId":1,
+"idBook": 3},
+
+{"name":"Управление проектами от А до Я",
+"authorName":"Ричард Ньютон &#169;",
+"price":675,"coverUrl":"http://45.8.249.57/public/6083509554.webp",
+"categoryId":2,
+"idBook": 4},
+
+{"name":"Чистый код: создание, анализ и рефакторинг",
+"authorName":"Роберт Мартин &#169;",
+"price":700,
+"coverUrl":"http://45.8.249.57/public/6189288048.webp",
+"categoryId":3,
+"idBook": 5},
+
+{"name":"Бизнес с нуля",
+"authorName":"Эрик Рис &#169;",
+"price":920,
+"coverUrl":"http://45.8.249.57/public/6304182856.webp",
+"categoryId":2,
+"idBook": 6},
+
+{"name":"Идеальный программист",
+"authorName":"Роберт Мартин &#169;",
+"price":1020,"coverUrl":"http://45.8.249.57/public/6264960718.webp",
+"categoryId":3,
+"idBook": 7},
+
+{"name":"Не заставляйте меня думать",
+"authorName":"Стив Круг &#169;",
+"price":1169,
+"coverUrl":"http://45.8.249.57/public/6053518393.webp",
+"categoryId":1,
+"idBook": 8},
+
+{"name":"Как пасти котов",
+"authorName":"Дж.Ханк Рейнвотер &#169;",
+"price":1207,
+"coverUrl":"http://45.8.249.57/public/6277282274.webp",
+"categoryId":2,
+"idBook": 9},
+
+{"name":"Пять пороков команды",
+"authorName":"Патрик Ленсиони &#169;",
+"price":1649,"coverUrl":"http://45.8.249.57/public/6206226336.webp",
+"categoryId":2,
+"idBook": 10},
+
+{"name":"Совершенный код",
+"authorName":"Стив Макконнелл &#169;","price":1787,
+"coverUrl":"http://45.8.249.57/public/6190014646.webp",
+"categoryId":3,
+"idBook": 11},
+
+{"name":"Scrum. Революционный метод управления проектами",
+"authorName":"Джефф Сазерленд &#169;",
+"price":1937,
+"coverUrl":"http://45.8.249.57/public/6280422562.webp",
+"categoryId":2,
+"idBook": 12},
+
+{"name":"Модульные системы в графическом дизайне",
+"authorName":"Йозеф Мюллер-Брокманн &#169;",
+"price":2048,
+"coverUrl":"http://45.8.249.57/public/6252844739.webp",
+"categoryId":1,
+"idBook": 13},
+
+{"name":"Deadline. Роман об управлении проектами",
+"authorName":"Том Демарко &#169;",
+"price":2448,
+"coverUrl":"http://45.8.249.57/public/6277899275.webp",
+"categoryId":2,
+"idBook": 14},
+
+
+{"name":"Код: тайный язык информатики",
+"authorName":"Чарльз Петцольд &#169;",
+"price":2479,"coverUrl":"http://45.8.249.57/public/6292446592.webp",
+"categoryId":3,
+"idBook": 15},
+
+{"name":"Программист-прагматик","authorName":"Дэвид Томас, Эндрю Хант &#169;",
+"price":3495,
+"coverUrl":"http://45.8.249.57/public/6016016523.webp",
+"categoryId":3,
+"idBook": 16},
+
+{"name":"Шаблоны корпоративных приложений","authorName":"Мартин Фаулер &#169;",
+"price":4295,
+"coverUrl":"http://45.8.249.57/public/6225046249.webp",
+"categoryId":3,
+"idBook": 17},
+
+{"name":"Рефакторинг. Улучшение проекта, существующего кода",
+"authorName":"Мартин Фаулер &#169;",
+"price":7125,
+"coverUrl":"http://45.8.249.57/public/6114690274.webp",
+"categoryId":3,
+"idBook": 18},
+
+{"name":"Алгоритмы. Построение и анализ",
+"authorName":"Томас Кормен, Чарльз Лейзерсон, Рональд Ривест, Клиффорд Штайн &#169;",
+"price":16875,"coverUrl":"http://45.8.249.57/public/6120969471.webp",
+"categoryId":3,
+"idBook": 19}]
+
+let bodyBoxBooks = document.querySelector('.box-books-all');
+
+
+
+arrBoks.forEach(e => bodyBoxBooks.innerHTML += `<div class="box-book card card${e.idBook}" data-id="0${e.idBook}">
+<img
+  class="book_img"
+  src="${e.coverUrl}"
+  alt="book"
+/>
+<div class="book_block">
+  <h1 class="book_name card2">
+  ${e.name}
+  </h1>
+  <h2 class="book_aut">${e.authorName}</h2>
+  <div class="book-rub-btn">
+      <p class="book_price">${e.price}&#8381;</p>
+  <button data-cart type="button" class="book_btn btn btn-block btn-outline-warning">В корзину</button>
+
+  </div>
+  
+</div>
+</div>`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////$Recycle.Bin
+
+let arrCategories = [
+    {"id":1,"name":"Дизайн"},
+    {"id":2,"name":"Управление проектами"},
+    {"id":3,"name":"Разработка"},
+    {"id":4,"name":"Тестирование"}];
+
+    ////////////////////////////////////////////////////////////////////////////////////
+
+let opi = document.getElementById('opi');
+let card = document.querySelector('.card');
+let selectt = document.getElementById('seelcttt'); 
+let boxBook = document.querySelectorAll('.box-book');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function fun1() {
+var sel=document.getElementById('seelcttt').selectedIndex;
+var options=document.getElementById('seelcttt').options;
+
+    
+if(options[sel].value == 'diz'){
+
+  if(document.getElementById('sortmin') != null){
+    document.getElementById('sortmin').id = 'sortminDiz';
+    } else if (document.getElementById('sortminUpr') != null){
+      document.getElementById('sortminUpr').id = 'sortminDiz';
+    } else if (document.getElementById('sortminRaz') != null){
+      document.getElementById('sortminRaz').id = 'sortminDiz';
+    } 
+  
+  sortminDiz.addEventListener('click', function(){
+    const reversedDiz = arrBoksDiz.reverse();
+  
+    bodyBoxBooks.innerHTML = ''
+    reversedDiz.forEach(e => bodyBoxBooks.innerHTML += `<div class="box-book card card${e.idBook}" data-id="0${e.idBook}">
+  <img
+  class="book_img"
+  src="${e.coverUrl}"
+  alt="book"
+  />
+  <div class="book_block">
+  <h1 class="book_name card2">
+  ${e.name}
+  </h1>
+  <h2 class="book_aut">${e.authorName}</h2>
+  <div class="book-rub-btn">
+      <p class="book_price">${e.price}&#8381;</p>
+  <button data-cart class="book_btn">В корзину</button>
+  </div>
+  
+  </div>
+  </div>`);
+  })
+
+  
+bodyBoxBooks.innerHTML = '';
+
+    arrBoksDiz.forEach(e => bodyBoxBooks.innerHTML += `<div class="box-book card card${e.idBook}" data-id="0${e.idBook}">
+<img
+  class="book_img"
+  src="${e.coverUrl}"
+  alt="book"
+/>
+<div class="book_block">
+  <h1 class="book_name card2">
+  ${e.name}
+  </h1>
+  <h2 class="book_aut">${e.authorName}</h2>
+  <div class="book-rub-btn">
+      <p class="book_price">${e.price}&#8381;</p>
+  <button data-cart class="book_btn">В корзину</button>
+  </div>
+  
+</div>
+</div>`);
+
+        
+     
+} else if (options[sel].value == 'upr'){
+
+  if(document.getElementById('sortmin') != null){
+    document.getElementById('sortmin').id = 'sortminUpr';
+    } else if (document.getElementById('sortminRaz') != null){
+      document.getElementById('sortminRaz').id = 'sortminUpr';
+    } else if (document.getElementById('sortminDiz') != null){
+      document.getElementById('sortminDiz').id = 'sortminUpr';
+    }else document.getElementById('sortminRaz').id = 'sortminUpr';
+      
+    
+
+  
+    let sortminUpr = document.getElementById('sortminUpr')
+
+    sortminUpr.addEventListener('click', function(){
+      const reversedUpr = arrBoksUpr.reverse();
+    
+      bodyBoxBooks.innerHTML = ''
+      reversedUpr.forEach(e => bodyBoxBooks.innerHTML += `<div class="box-book card card${e.idBook}" data-id="0${e.idBook}">
+    <img
+    class="book_img"
+    src="${e.coverUrl}"
+    alt="book"
+    />
+    <div class="book_block">
+    <h1 class="book_name card2">
+    ${e.name}
+    </h1>
+    <h2 class="book_aut">${e.authorName}</h2>
+    <div class="book-rub-btn">
+        <p class="book_price">${e.price}&#8381;</p>
+    <button data-cart class="book_btn">В корзину</button>
+    </div>
+    
+    </div>
+    </div>`);
+    })
+
+    
+    bodyBoxBooks.innerHTML = '';
+
+    arrBoksUpr.forEach(e => bodyBoxBooks.innerHTML += `<div class="box-book card card${e.idBook}" data-id="0${e.idBook}">
+<img
+  class="book_img"
+  src="${e.coverUrl}"
+  alt="book"
+/>
+<div class="book_block">
+  <h1 class="book_name card2">
+  ${e.name}
+  </h1>
+  <h2 class="book_aut">${e.authorName}</h2>
+  <div class="book-rub-btn">
+      <p class="book_price">${e.price}&#8381;</p>
+  <button data-cart class="book_btn">В корзину</button>
+  </div>
+  
+</div>
+</div>`);
+    
+} else if (options[sel].value == 'raz'){
+
+  if(document.getElementById('sortmin') != null){
+    document.getElementById('sortmin').id = 'sortminRaz';
+    } else if (document.getElementById('sortminUpr') != null){
+      document.getElementById('sortminUpr').id = 'sortminRaz';
+    } 
+    else if (document.getElementById('sortminDiz') != null){
+      document.getElementById('sortminDiz').id = 'sortminRaz';
+    }else document.getElementById('sortminRaz').id = 'sortminRaz';
+
+    let sortminRaz = document.getElementById('sortminRaz')
+
+
+    sortminRaz.addEventListener('click', function(){
+      const reversedRaz = arrBoksRaz.reverse();
+    
+      bodyBoxBooks.innerHTML = ''
+      reversedRaz.forEach(e => bodyBoxBooks.innerHTML += `<div class="box-book card card${e.idBook}" data-id="0${e.idBook}">
+    <img
+    class="book_img"
+    src="${e.coverUrl}"
+    alt="book"
+    />
+    <div class="book_block">
+    <h1 class="book_name card2">
+    ${e.name}
+    </h1>
+    <h2 class="book_aut">${e.authorName}</h2>
+    <div class="book-rub-btn">
+        <p class="book_price">${e.price}&#8381;</p>
+    <button data-cart class="book_btn">В корзину</button>
+    </div>
+    
+    </div>
+    </div>`);
+    })
+
+
+
+    bodyBoxBooks.innerHTML = '';
+
+    arrBoksRaz.forEach(e => bodyBoxBooks.innerHTML += `<div class="box-book card card${e.idBook}" data-id="0${e.idBook}">
+<img
+  class="book_img"
+  src="${e.coverUrl}"
+  alt="book"
+/>
+<div class="book_block">
+  <h1 class="book_name card2">
+  ${e.name}
+  </h1>
+  <h2 class="book_aut">${e.authorName}</h2>
+  <div class="book-rub-btn">
+      <p class="book_price">${e.price}&#8381;</p>
+  <button data-cart class="book_btn">В корзину</button>
+  </div>
+  
+</div>
+</div>`);
+
+} else if ((options[sel].value == 'kat')) {
+    bodyBoxBooks.innerHTML = ''
+    arrBoks.forEach(e => bodyBoxBooks.innerHTML += `<div class="box-book card card${e.idBook}" data-id="0${e.idBook}">
+<img
+  class="book_img"
+  src="${e.coverUrl}"
+  alt="book"
+/>
+<div class="book_block">
+  <h1 class="book_name card2">
+  ${e.name}
+  </h1>
+  <h2 class="book_aut">${e.authorName}</h2>
+  <div class="book-rub-btn">
+      <p class="book_price">${e.price}&#8381;</p>
+  <button data-cart class="book_btn">В корзину</button>
+  </div>
+  
+</div>
+</div>`);
+} else {
+    bodyBoxBooks.innerHTML = `<h1>Пока книг по тестировнию нет :)</h1>`
+}
+}
+
+
+let sortup = document.getElementById('sortup')
+let sortmin = document.getElementById('sortmin')
+
+
+
+sortmin.addEventListener('click', function(){
+  const reversed = arrBoks.reverse();
+
+  bodyBoxBooks.innerHTML = ''
+  reversed.forEach(e => bodyBoxBooks.innerHTML += `<div class="box-book card card${e.idBook}"  data-id="0${e.idBook}">
+<img
+class="book_img"
+src="${e.coverUrl}"
+alt="book"
+/>
+<div class="book_block">
+<h1 class="book_name card2">
+${e.name}
+</h1>
+<h2 class="book_aut">${e.authorName}</h2>
+<div class="book-rub-btn">
+    <p class="book_price">${e.price}&#8381;</p>
+<button data-cart class="book_btn">В корзину</button>
+</div>
+
+</div>
+</div>`);
+})
+
+
+
+
+
+
+
+
+
+
