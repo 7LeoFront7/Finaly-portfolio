92e1189490cdde6a0b0187f146e99f584e4908cb
Максим <98467035+7LeoFront7@users.noreply.github.com>
2023-05-26 15:39:05 +0300

    Add files via upload

--- works/NewIzum/js/main.js
+++ works/NewIzum/js/main.js
@@ -1,3 +1,63 @@
+
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/embed/d7ocgBD6WVU";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+
+
+
+$(document).ready(function() {
+    $('#video-modal').on('shown.bs.modal', function(e) {
+        let button = $(e.relatedTarget),
+            video = button.data('video');
+
+        if (player) {
+            if (player.playerInfo.videoData.video_id == video) {
+                player.playVideo();
+            } else {
+                player.loadVideoById(video);
+            }
+
+        } else {
+            player = new YT.Player('player', {
+
+                playerVars: {
+                    'controls': 0,
+                    'showinfo': 0,
+                    'rel': 0,
+                    'autoplay': 0,
+                    'playsinline': 1
+                },
+                videoId: video,
+                events: {
+                    'onReady': onPlayerReady,
+                }
+            });
+        }
+    });
+
+    $('#video-modal').on('hidden.bs.modal', function() {
+        player.pauseVideo();
+    });
+
+    function onPlayerReady() {
+        player.playVideo();
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
 const items = document.querySelectorAll(".accordion button");
 
 function toggleAccordion() {
@@ -23,3 +83,38 @@ let burgerPhone = document.querySelector('.burger-phone')
 headerBurger.addEventListener('click', function(){
     burgerPhone.classList.toggle('open-burger')
 })
+
+////////////////////////////////////////////////////////////////////
+
+
+//$( "#submitAmo" ).on( "click", function() {
+  //  $('.amoforms_iframe').css('display','block');
+  //} );
+  
+
+
+
+  let btnVidosA = document.querySelector('.home__video-izum')
+  
+  
+  let actifeFormZakaz = document.querySelector('.actife__form-zakaz')
+  let clousefornacmZakaz = document.querySelector('.clousefornacm-zakaz')
+  
+  let actifeForm = document.querySelector('.actife__form')
+  let clousefornacm = document.querySelector('.clousefornacm')
+  let headerOther = document.querySelector('.header__other')
+  
+  
+  
+  headerOther.addEventListener('click', function(){
+      actifeFormZakaz.classList.toggle('actife__form-open-zakaz')
+  })
+  
+  clousefornacmZakaz.addEventListener('click', function(){
+      actifeFormZakaz.classList.remove('actife__form-open-zakaz')
+  })
+  
+
+///////////////////////////////////////////////////////////////////////
+
+  
