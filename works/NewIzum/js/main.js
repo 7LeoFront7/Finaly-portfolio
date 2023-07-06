8fc53c9bfcefa58626aa733caf36663465701082
Максим <98467035+7LeoFront7@users.noreply.github.com>
2023-07-06 16:10:01 +0300

    Update main.js

--- works/NewIzum/js/main.js
+++ works/NewIzum/js/main.js
@@ -17,6 +17,8 @@ function toggleAccordion(){
 }
 items.forEach(item => item.addEventListener('click', toggleAccordion));
 
+////////////////////////////////
+
 let Zapis1BTN = document.querySelector('.Zapis1BTN')
 let wrapperOnlineZapis = document.querySelector('.wrapperOnlineZapis1')
 let closeZapis1 = document.querySelector('.closeZapis1')
@@ -27,4 +29,22 @@ Zapis1BTN.addEventListener('click', function(){
 
 closeZapis1.addEventListener('click', function(){
   wrapperOnlineZapis.classList.remove('wrapperOnlineZapis-opening')
+})
+
+////////////////////////////////
+
+let ZakazZvon1 = document.querySelector('.ZakazZvon1')
+let ZakazZvon2 = document.querySelector('.ZakazZvon2')
+let wrapperZakazZvon1 = document.querySelector('.wrapperZakazZvon1')
+let closeZakazZvon1 = document.querySelector('.closeZakazZvon1')
+
+ZakazZvon1.addEventListener('click', function(){
+  wrapperZakazZvon1.classList.add('wrapperZakazZvon1-opening')
+})
+ZakazZvon2.addEventListener('click', function(){
+  wrapperZakazZvon1.classList.add('wrapperZakazZvon1-opening')
+})
+
+closeZakazZvon1.addEventListener('click', function(){
+  wrapperZakazZvon1.classList.remove('wrapperZakazZvon1-opening')
 })
