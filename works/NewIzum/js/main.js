844ea27f1ec1109e5408d8b83c7ee58723dcd363
Максим <98467035+7LeoFront7@users.noreply.github.com>
2023-07-06 15:08:06 +0300

    Update main.js

--- works/NewIzum/js/main.js
+++ works/NewIzum/js/main.js
@@ -16,3 +16,15 @@ function toggleAccordion(){
   this.nextElementSibling.classList.toggle('active');
 }
 items.forEach(item => item.addEventListener('click', toggleAccordion));
+
+let Zapis1BTN = document.querySelector('.Zapis1BTN')
+let wrapperOnlineZapis = document.querySelector('.wrapperOnlineZapis1')
+let closeZapis1 = document.querySelector('.closeZapis1')
+
+Zapis1BTN.addEventListener('click', function(){
+  wrapperOnlineZapis.classList.add('wrapperOnlineZapis-opening')
+})
+
+closeZapis1.addEventListener('click', function(){
+  wrapperOnlineZapis.classList.remove('wrapperOnlineZapis-opening')
+})
